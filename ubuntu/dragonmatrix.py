cb7ddfd69a7ae04073c273e729cdaa281aa7cb18
twixxqz <115325310+twixxqz@users.noreply.github.com>
2022-10-08 12:40:34 +0300

    Add files via upload

---
+++ ubuntu/dragonmatrix.py
@@ -0,0 +1,11 @@
+import os
+from time import sleep
+
+os.system('clear')
+os.system('sudo apt install cowsay')
+sleep(4)
+os.system('clear')
+os.system('cowsay -f dragon "Hello!"')
+sleep(5)
+os.system('sudo apt install cmatrix')
+os.system('cmatrix')
